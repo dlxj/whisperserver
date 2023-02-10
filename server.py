f872b013e5d72b4268d047dc006cc81ae436b49c
guandong <1623088350@qq.com>
2023-02-10 15:11:14 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,33 +1,29 @@
 
+import whisper
+model = whisper.load_model("medium")
 import glob
 
-mp3paths = glob.glob('E:/audios/*.mp3', recursive=True)
+mp3paths = glob.glob('./audios/*.mp3', recursive=True)
 for mp3path in mp3paths:
     textpath = mp3path.replace('audios', 'audios_text').replace('mp3', 'txt')
     with open(textpath, "r", encoding="utf-8") as fp:
         text = fp.read()
-        a = 1
 
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(mp3path)
+    audio = whisper.pad_or_trim(audio)
 
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-# import whisper
+    # detect the spoken language
+    # _, probs = model.detect_language(mel)
+    # print(f"Detected language: {max(probs, key=probs.get)}")
 
-# model = whisper.load_model("base")
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
 
-# # load audio and pad/trim it to fit 30 seconds
-# audio = whisper.load_audio("audio.mp3")
-# audio = whisper.pad_or_trim(audio)
-
-# # make log-Mel spectrogram and move to the same device as the model
-# mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-# # detect the spoken language
-# _, probs = model.detect_language(mel)
-# print(f"Detected language: {max(probs, key=probs.get)}")
-
-# # decode the audio
-# options = whisper.DecodingOptions()
-# result = whisper.decode(model, mel, options)
-
-# # print the recognized text
-# print(result.text)
+    # print the recognized text
+    print(f'predict: {result.text}')
+    print(f'origin : {text}')
